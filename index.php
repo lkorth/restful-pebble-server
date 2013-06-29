c63af3dea431bd6d5e07ff953783d5d6852a22ea
lkorth <luke@lukekorth.com>
2013-06-29 01:27:54 -0500

    Add simple curl function

--- index.php
+++ index.php
@@ -230,6 +230,17 @@ $app->post('/xmlrpc.php', function(Application $app) {
 	}
 });
 
+function get_data($url) {
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
 function success($innerXML) {
 	$xml =  <<<EOD
 <?xml version="1.0"?>
