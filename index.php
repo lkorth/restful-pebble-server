c9f5a14e9be8457583cf30d927e11c7ede564986
lkorth <luke@lukekorth.com>
2013-08-29 22:34:28 -0700

    Increase timeout to help with silent failures

--- index.php
+++ index.php
@@ -325,7 +325,7 @@ $app->post('/xmlrpc.php', function(Application $app) {
 
 function get_data($url) {
     $ch = curl_init();
-    $timeout = 5;
+    $timeout = 25;
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
